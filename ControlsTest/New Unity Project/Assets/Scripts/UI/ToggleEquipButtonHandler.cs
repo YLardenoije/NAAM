8b416e6c1d186780def9222ac625aeb784a3b55e
Mern <mar-vis10@hotmail.com>
2019-09-13 12:14:31 +0200

    Ui is a thing but does not work lol

---
+++ ControlsTest/New Unity Project/Assets/Scripts/UI/ToggleEquipButtonHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent (typeof (Button))]
+public class ToggleEquipButtonHandler : MonoBehaviour
+{
+    [SerializeField] private GlobalData GlobalData;
+    private Text[] Texts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Texts = GetComponentsInChildren<Text>();
+        GlobalData.SelectedItemType = GlobalData.ItemTypes.MovementItem;
+        Texts[(int)GlobalData.ItemTypes.CombatItem].enabled = true;
+        Texts[(int)GlobalData.ItemTypes.MovementItem].enabled = false;
+    }
+
+    public void OnButtonPress()
+    {
+        //toggle the enable status of both text fields
+        Texts[(int)GlobalData.ItemTypes.CombatItem].enabled ^= true;
+        Texts[(int)GlobalData.ItemTypes.MovementItem].enabled ^= true;
+
+        //switch the equipped item type
+        if( GlobalData.SelectedItemType == GlobalData.ItemTypes.MovementItem )
+        {
+            GlobalData.SelectedItemType = GlobalData.ItemTypes.CombatItem;
+        }
+        else
+        {
+            GlobalData.SelectedItemType = GlobalData.ItemTypes.MovementItem;
+        }
+        
+    }
+    
+
+}
