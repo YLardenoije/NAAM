55ae644e59a7f9de8e333a0c739b7b7edfd8cf6a
yves lardenoije <yveslardenoije@gmail.com>
2019-09-26 11:10:46 +0200

    with adaptive music

---
+++ ControlsTest/New Unity Project/Assets/FMODParameterEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD;
+using FMODUnity;
+
+public class FMODParameterEditor : MonoBehaviour
+{
+    public int Intensity=0;
+    private float threat;
+    FMOD.Studio.EventInstance FmodEv;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FmodEv = GetComponent<StudioEventEmitter>().EventInstance;
+        FmodEv.getParameterByName("ThreatLevel", out threat);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FmodEv.setParameterByName("ThreatLevel",(float)Intensity);
+    }
+}
