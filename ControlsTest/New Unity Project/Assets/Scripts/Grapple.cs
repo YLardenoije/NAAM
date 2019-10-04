e658f2aa2cd48961bdd62b1f7b71c949507ebac2
Mern <mar-vis10@hotmail.com>
2019-10-04 16:01:38 +0200

    can toggle fire mode now. Fireballs spawn, but hit the source. This is a bug.

--- ControlsTest/New Unity Project/Assets/Scripts/Grapple.cs
+++ ControlsTest/New Unity Project/Assets/Scripts/Grapple.cs
@@ -8,6 +8,7 @@ public class Grapple : MonoBehaviour
     public Vector2 Target; // where the grapple is aimed
     private bool Attached; // whether the grapple actually hit or not
     [SerializeField] private GameObject HitObject; // if the grapple hit something, find its reference here. otherwise null.
+    private Vector3 HitLoc;
     [SerializeField] private float GrapplePower; // how strong the grapple pulls.
     private bool HitObjectIsEnemy;
     private Projectile projectile;
@@ -21,6 +22,7 @@ public class Grapple : MonoBehaviour
         Attached = false;
         HitObject = null;
         HitObjectIsEnemy = false;
+        
     }
 
     
@@ -43,6 +45,7 @@ public class Grapple : MonoBehaviour
             {
                 Source.gameObject.GetComponent<Rigidbody2D>().AddForce(
                     (transform.position - Source.gameObject.transform.position).normalized * GrapplePower, ForceMode2D.Impulse);
+                transform.position = HitLoc;
             }
         }
     }
@@ -56,6 +59,7 @@ public class Grapple : MonoBehaviour
             Debug.Log("Hit an object!");
             Attached = true;
             HitObject = col.gameObject;
+            HitLoc = transform.position;
             if( HitObject.GetComponent<Enemy>() != null )
             {
                 HitObjectIsEnemy = true;
