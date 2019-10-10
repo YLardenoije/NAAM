181b63622be1f7ac7fa565eb9cfdb04720bf265c
Mern <mar-vis10@hotmail.com>
2019-10-10 15:20:47 +0200

    UI fixes

--- ControlsTest/New Unity Project/Assets/Scripts/Player.cs
+++ ControlsTest/New Unity Project/Assets/Scripts/Player.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
+    public UnityEvent OnPlayerDeath;
+
     [SerializeField] GlobalData GlobalData;
+
+    private LivingThing livingThing;
     // Start is called before the first frame update
     void Start()
     {
         GlobalData.player = this;
+        livingThing = gameObject.GetComponent<LivingThing>();
+        livingThing.OnDeathEvent.AddListener(OnDeath);
+        OnPlayerDeath = new UnityEvent();
     }
 
     // Update is called once per frame
@@ -19,7 +27,8 @@ public class Player : MonoBehaviour
 
     public void OnDeath()
     {
-
+        OnPlayerDeath.Invoke();
+        Destroy(gameObject);
     }
 
     private void OnDestroy()
---
+++ ControlsTest/New Unity Project/Assets/Scripts/UI/GameEventHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEventHandler : MonoBehaviour
+{
+    [SerializeField] private GlobalData GlobalData;
+    [SerializeField] private Canvas Canvas;
+
+    private Player player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GlobalData.player;
+        player.OnPlayerDeath.AddListener(OnPlayerDeath);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPlayerWin()
+    {
+
+    }
+
+    public void OnPlayerDeath()
+    {
+        GameObject[] GameObjects;
+        GameObjects = Canvas.GetComponents<GameObject>();
+        GameObjects[0].SetActive(false);
+        GameObjects[1].SetActive(true);
+    }
+}
