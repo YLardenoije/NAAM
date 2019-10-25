eb12427f364a1d76bcc1d62cb3816b45d38267f4
Mern <mar-vis10@hotmail.com>
2019-10-25 11:01:00 +0200

    Tutorial isnt done but its honest work

---
+++ ControlsTest/New Unity Project/Assets/Scripts/Tutorial/Goal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Goal : MonoBehaviour
+{
+    public UnityEvent GoalReached = new UnityEvent();
+    [SerializeField] private Spawner NextSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PlayerCollision( Player player )
+    {
+        player.transform.position = NextSpawn.transform.position;
+        GoalReached.Invoke();
+    }
+}
---
+++ ControlsTest/New Unity Project/Assets/Scripts/Tutorial/TutorialBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialBehaviour : MonoBehaviour
+{
+    [SerializeField] private Spawner TutorialStartPoint;
+    [SerializeField] private Player PlayerPrefab;
+    [SerializeField] private int TutorialProgress;
+
+    private Player player;
+    private List<Goal> Goals;  
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = Instantiate(PlayerPrefab, TutorialStartPoint.transform.position, PlayerPrefab.transform.rotation);
+        Goals = new List<Goal>();
+        TutorialProgress = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch( TutorialProgress )
+        {
+            case 0:
+                Goals.AddRange( FindObjectsOfType<Goal>() );
+                break;
+        }
+    }
+}
