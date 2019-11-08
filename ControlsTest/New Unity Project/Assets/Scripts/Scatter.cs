d2ee2ecbb6a4e5f3113d152eaa9147d6107fae17
Mern <mar-vis10@hotmail.com>
2019-11-08 10:54:59 +0100

    nothing lol

---
+++ ControlsTest/New Unity Project/Assets/Scripts/Scatter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scatter : MonoBehaviour
+{
+    public Vector2 Target;
+    public GameObject Source;
+    private Projectile projectile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
